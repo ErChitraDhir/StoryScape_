049479517f619b9f53ce58eae7d8496d68d307d8
Chitra Dhir <er.chitra.dhir@gmail.com>
2024-10-25 10:55:50 +0530

    render.

--- story-theme/backend/app.js
+++ story-theme/backend/app.js
@@ -1,20 +1,34 @@
-const express = require("express")
-const app = express()
-const port = process.env.PORT || 5000
-const config = require("./config/db")
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const config = require("./config/db");
+const cors = require("cors");
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.urlencoded({extended:false}))
-app.use(express.json({limit:'50mb'}))
-app.use(express.static(__dirname+("/public/")))
+const corsOptions = {
+    origin: 'https://storyscape.onrender.com', 
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.static(__dirname + "/public/"));
 
-const routes = require("./routes/apiRoutes")
-app.use("/api",routes)
+const routes = require("./routes/apiRoutes");
+app.use("/api", routes);
 
-const seeder = require('./config/seeder')
-seeder.adminseeder()
+// Seeder
+if (process.env.NODE_ENV !== 'production') {
+    const seeder = require('./config/seeder');
+    seeder.adminseeder();
+}
 
-app.listen(port,()=>{
-    console.log("server Connected")
-})
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log("Server Connected on port " + port);
+});
