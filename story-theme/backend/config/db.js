3e8e8a250120c6f74145a2e3391dc6b00f49e82a
Chitra Dhir <er.chitra.dhir@gmail.com>
2024-10-25 10:21:56 +0530

    initial commit.

--- story-theme/backend/config/db.js
+++ story-theme/backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/storyscape")
+mongoose.connect("mongodb+srv://admin:Database2024@cluster1.q8qtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
 .then(()=>{
     console.log("database connected")
 })
